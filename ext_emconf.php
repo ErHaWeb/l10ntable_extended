37ea64965abe9c7930193727d3bd4e7e6ecbf310
Eric Harrer <info@eric-harrer.de>
2023-09-07 17:40:09 +0200

    [TASK] Update extension meta files

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-12.99.99',
         ],
         'conflicts' => [
         ],
