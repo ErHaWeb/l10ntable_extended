fe46a9ead207026a2a621a8c5e28f0a8a1428181
Eric Harrer <info@eric-harrer.de>
2023-09-12 13:28:15 +0200

    [TASK] Update dependency information

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.99.99',
+            'typo3' => '11.5.0-12.4.99',
+            'info' => '11.5.0-12.4.99',
         ],
         'conflicts' => [
         ],
